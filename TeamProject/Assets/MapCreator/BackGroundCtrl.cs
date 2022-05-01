e5670c1276a3e9a912537e3a8ea4e7724d79fb72
BeomkiKim <bumki1226@gmail.com>
2022-05-01 18:06:07 +0900

    background Update
    거리에 따라 배경 바뀜

--- TeamProject/Assets/MapCreator/BackGroundCtrl.cs
+++ TeamProject/Assets/MapCreator/BackGroundCtrl.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class BackGroundCtrl : MonoBehaviour
 {
     MeshRenderer meshRender;
+    public Material[] backGroundMaterial;
+    Renderer backGroundRederer;
 
     public float speed;
     float offset;
-    // Start is called before the first frame update
+
+    [Header("플레이어거리")]
+    public float distance = 0f;
+
+    [Header("2라운드거리")]
+    public float secondDistance;
+
+    [Header("3라운드거리")]
+    public float thirdDistance;
     void Start()
     {
-        meshRender = GetComponent<MeshRenderer>();        
+        meshRender = GetComponent<MeshRenderer>();
+        backGroundRederer = GetComponent<Renderer>();
+        backGroundRederer.enabled = true;
+        backGroundRederer.sharedMaterial = backGroundMaterial[0];
     }
 
-    // Update is called once per frame
     void Update()
     {
         offset += Time.deltaTime * speed;
+        distance += Time.deltaTime;
         meshRender.material.mainTextureOffset = new Vector2(offset, 0);
+
+        if(distance >= secondDistance)
+        {
+            backGroundRederer.sharedMaterial = backGroundMaterial[1];
+            offset += Time.deltaTime * speed;
+            meshRender.material.mainTextureOffset = new Vector2(offset, 0);
+        }
+        if(distance >= thirdDistance)
+        {
+            backGroundRederer.sharedMaterial = backGroundMaterial[2];
+            offset += Time.deltaTime * speed;
+            meshRender.material.mainTextureOffset = new Vector2(offset, 0);
+        }
     }
 }
