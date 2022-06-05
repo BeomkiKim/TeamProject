ba5defd5e202f65d9eba66a38933efa4f2ff98e3
Hwangtaehun <r283@naver.com>
2022-06-05 18:18:08 +0900

    음량조절 씬 공유 적용
    음량조절 씬 공유 되지만 버그 있으면 말씀해주세요.

---
+++ TeamProject/Assets/2. Scripts/UIScripts/AudioManagerTutorial.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManagerTutorial : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectPref = "SoundEffectPref";
+
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectsSlider;
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource[] backgroundAudio;
+    public AudioSource[] soundEffectAudio;
+
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt == 0)
+        {
+            backgroundFloat = 0.125f;
+            soundEffectsFloat = 0.75f;
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsSlider.value = soundEffectsFloat;
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectPref, soundEffectsFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+          backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+          backgroundSlider.value = backgroundFloat;
+          soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectPref);
+          soundEffectsSlider.value = soundEffectsFloat;
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectPref, soundEffectsSlider.value);
+    }
+
+    void OnApplicationFocus(bool infocus)
+    {
+        if (!infocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void UpdateSound()
+    {
+        for(int i = 0; i<backgroundAudio.Length; i++)
+        {
+            backgroundAudio[i].volume = backgroundSlider.value;
+        }
+
+        for(int j = 0; j<soundEffectAudio.Length; j++)
+        {
+            soundEffectAudio[j].volume = soundEffectsSlider.value;
+        }
+    }
+}
--- TeamProject/Assets/2. Scripts/UIScripts/UI_Sound.cs
+++ TeamProject/Assets/2. Scripts/UIScripts/UI_Sound.cs
@@ -4,60 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 
-
 public class UI_Sound : MonoBehaviour
 {
-    //[SerializeField] Slider BGMSlider;
-    //[SerializeField] Slider SoundEffectSlider;
-
     AudioSource sound;
-    public AudioMixer mixer;
 
     void Start()
     {
         sound = GetComponent<AudioSource>();
-
-        //if (!PlayerPrefs.HasKey("BGM"))
-        //{
-        //    PlayerPrefs.SetFloat("BGM", 1);
-        //    Load();
-        //}
-        //else if (!PlayerPrefs.HasKey("SoundEffect"))
-        //{
-        //    PlayerPrefs.SetFloat("SoundEffect", 1);
-        //    Load();
-        //}
-        //else
-        //{
-        //    Load();
-        //}
-
     }
-
-    public void BGMSetLevel(float sliderVal)
-    {
-        mixer.SetFloat("BGM", Mathf.Log10(sliderVal) * 20);
-        //Save();
-    }
-
-    public void SoundEffectSetLevel(float sliderVal)
-    {
-        mixer.SetFloat("SoundEffect", Mathf.Log10(sliderVal) * 20);
-        //Save();
-    }
-
-    //private void Load()
-    //{
-    //    BGMSlider.value = PlayerPrefs.GetFloat("BGM");
-    //    SoundEffectSlider.value = PlayerPrefs.GetFloat("SoundEffect");
-    //}
-
-    //private void Save()
-    //{
-    //    PlayerPrefs.SetFloat("BGM", BGMSlider.value);
-    //    PlayerPrefs.SetFloat("SoundEffect", SoundEffectSlider.value);
-
-    //}
 
     public void MutePush()
     {
