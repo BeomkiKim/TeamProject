d6c29f206a62ef415c32a772a6f07d0d9e60ba41
Hwangtaehun <r283@naver.com>
2022-06-05 16:05:04 +0900

    종료버튼과 오프닝 씬에 게임설정 추가
    종료 버튼과 오프닝 씬에 게임설정 추가했습니다.

--- TeamProject/Assets/2. Scripts/UIScripts/UI_Sound.cs
+++ TeamProject/Assets/2. Scripts/UIScripts/UI_Sound.cs
@@ -7,23 +7,57 @@ using UnityEngine.Audio;
 
 public class UI_Sound : MonoBehaviour
 {
+    //[SerializeField] Slider BGMSlider;
+    //[SerializeField] Slider SoundEffectSlider;
+
     AudioSource sound;
     public AudioMixer mixer;
 
-    public void BGMSetLevel(float sliderVal) 
+    void Start()
     {
-        mixer.SetFloat("BGM", Mathf.Log10(sliderVal)*20);
+        sound = GetComponent<AudioSource>();
+
+        //if (!PlayerPrefs.HasKey("BGM"))
+        //{
+        //    PlayerPrefs.SetFloat("BGM", 1);
+        //    Load();
+        //}
+        //else if (!PlayerPrefs.HasKey("SoundEffect"))
+        //{
+        //    PlayerPrefs.SetFloat("SoundEffect", 1);
+        //    Load();
+        //}
+        //else
+        //{
+        //    Load();
+        //}
+
+    }
+
+    public void BGMSetLevel(float sliderVal)
+    {
+        mixer.SetFloat("BGM", Mathf.Log10(sliderVal) * 20);
+        //Save();
     }
 
     public void SoundEffectSetLevel(float sliderVal)
     {
         mixer.SetFloat("SoundEffect", Mathf.Log10(sliderVal) * 20);
+        //Save();
     }
 
-    void Start()
-    {
-        sound = GetComponent<AudioSource>();
-    }
+    //private void Load()
+    //{
+    //    BGMSlider.value = PlayerPrefs.GetFloat("BGM");
+    //    SoundEffectSlider.value = PlayerPrefs.GetFloat("SoundEffect");
+    //}
+
+    //private void Save()
+    //{
+    //    PlayerPrefs.SetFloat("BGM", BGMSlider.value);
+    //    PlayerPrefs.SetFloat("SoundEffect", SoundEffectSlider.value);
+
+    //}
 
     public void MutePush()
     {
